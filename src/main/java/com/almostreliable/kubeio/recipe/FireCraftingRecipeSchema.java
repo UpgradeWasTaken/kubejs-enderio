149b5704a2e2ecb6e6ae5f53853740e501d3d8dc
Relentless <relentless@rlnt.dev>
2023-09-09 14:50:57 +0200

    add utility method for adding single dimension to fire crafting

--- src/main/java/com/almostreliable/kubeio/recipe/FireCraftingRecipeSchema.java
+++ src/main/java/com/almostreliable/kubeio/recipe/FireCraftingRecipeSchema.java
@@ -4,9 +4,11 @@ import com.almostreliable.kubeio.KubeIOComponents;
 import com.enderio.base.common.recipe.FireCraftingRecipe;
 import com.enderio.base.data.recipe.FireCraftingRecipes;
 import com.mojang.datafixers.util.Either;
+import dev.latvian.mods.kubejs.recipe.RecipeJS;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
 import dev.latvian.mods.kubejs.recipe.component.StringComponent;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import org.apache.commons.lang3.ArrayUtils;
@@ -24,8 +26,18 @@ public interface FireCraftingRecipeSchema {
         .alwaysWrite();
 
     RecipeSchema SCHEMA = new RecipeSchema(
+        FireCraftingRecipeJS.class,
+        FireCraftingRecipeJS::new,
         BASE_BLOCKS,
         LOOT_TABLE,
         DIMENSIONS
     );
+
+    class FireCraftingRecipeJS extends RecipeJS {
+
+        public FireCraftingRecipeJS dimension(ResourceLocation dimension) {
+            setValue(DIMENSIONS, ArrayUtils.toArray(dimension.toString()));
+            return this;
+        }
+    }
 }
